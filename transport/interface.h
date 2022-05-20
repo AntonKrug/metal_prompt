40c989d22d16bcfc9801de37a95aa87929a80f17
Anton Krug <antonkrug@antonkrug.eu>
2022-05-20 20:10:07 +0100

    Elaborating on the #pragma mark

--- transport/interface.h
+++ transport/interface.h
@@ -10,7 +10,7 @@
 
 #include <stdbool.h>
 
-#pragma mark Public functions
+#pragma mark Public functions for generic transport interface
 extern void m_p_transport_out(const char* text);
 extern void m_p_transport_out_characters(const char* characters, uint32_t count);
 extern void m_p_transport_out_ln(void);
