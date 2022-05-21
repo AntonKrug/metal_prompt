febcab23798245ed5f382af0e70d27c6d7a17194
Anton Krug <antonkrug@antonkrug.eu>
2022-05-21 20:52:59 +0100

    Shrinking memory requirements

--- config.h
+++ config.h
@@ -9,7 +9,7 @@
 #define METAL_PROMPT_CONFIG_H_
 
 #define M_P_COMMAND_PROMPT "metal_prompt:/$ "
-#define M_P_COMMAND_NAME_LIMIT 256
+#define M_P_COMMAND_NAME_LIMIT 128
 #define M_P_FORCE_OPTIMIZATION __attribute__((optimize("-Os")))
 //#define M_P_FORCE_OPTIMIZATION // uncomment this line instead for no optimizations
 #define M_P_ALLOW_QUIT
@@ -17,7 +17,7 @@
 #define M_P_COLOR_ENABLE
 
 #ifdef M_P_RETURN_AND_ARGUMENT_STRING_ENABLE
-#define M_P_RETURN_AND_ARGUMENT_STRING_BUFFER_SIZE 64
+#define M_P_RETURN_AND_ARGUMENT_STRING_BUFFER_SIZE 48
 #endif
 
 // Comment this out when you want the list and it's arguments be triggered
