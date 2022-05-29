7072515c11657e73c4f5ce4bf07f5f0c901ffea0
Anton Krug <antonkrug@antonkrug.eu>
2022-05-29 13:12:52 +0100

    Restructuring the implementations

--- transport/pf_fabric_core_uart_apb/implementation.c
+++ transport/pf_fabric_core_uart_apb/implementation.c
@@ -5,25 +5,26 @@
  */
 
 /*
- * This is a transport implementation for Microchip - PolarFire SoC
- * devices which can use the microchip subsystem (mss) UART peripherals
+ * This is a transport implementation for Microchip - PolarFire
+ * (and SmartFusion2) devices which can have instantiated CoreUART_apb
+ * peripheral as a soft IP inside the FPGA fabric.
  */
 
-#include "pfsoc_mss_uart.h"
+#include "implementation.h"
 
-#include "interface.h"
+#include "metal_prompt/transport/interface.h"
 #include "metal_prompt/config.h"
 #include "metal_prompt/metal_prompt.h"
 
 #include <stdint.h>
 
 
-mss_uart_instance_t* uart_instance;
+UART_instance_t* uart_instance;
 
 #pragma mark - Public functions
 
 M_P_FORCE_OPTIMIZATION
-void m_p_prompt(mss_uart_instance_t* port_to_be_used) {
+void m_p_prompt(UART_instance_t* port_to_be_used) {
     uart_instance = port_to_be_used;
 
     m_p_prompt_generic();
@@ -35,13 +36,13 @@ void m_p_prompt(mss_uart_instance_t* port_to_be_used) {
 
 M_P_FORCE_OPTIMIZATION
 void m_p_transport_out(const char* text) {
-    MSS_UART_polled_tx_string(uart_instance, (const uint8_t*)text);
+    UART_polled_tx_string(uart_instance, (const uint8_t*)text);
 }
 
 
 M_P_FORCE_OPTIMIZATION
 void m_p_transport_out_characters(const char* characters, unsigned int count) {
-    MSS_UART_polled_tx(uart_instance, (const uint8_t*)characters, (uint32_t)count);
+    UART_fill_tx_fifo(uart_instance, (const uint8_t*)characters, (uint32_t)count);
 }
 
 
@@ -53,7 +54,7 @@ void m_p_transport_out_ln(void) {
 
 M_P_FORCE_OPTIMIZATION
 bool m_p_transport_in(char* out_buffer) {
-    uint32_t rx_count = MSS_UART_get_rx(uart_instance, out_buffer, 1);
+    uint32_t rx_count = UART_get_rx(uart_instance, out_buffer, 1);
 
     return (rx_count > 0);
 }
