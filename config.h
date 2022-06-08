266db34ba853b3bae681c4d295dc9ef00033c656
Anton Krug <antonkrug@antonkrug.eu>
2022-06-08 10:35:54 +0100

    Reorg of the config

--- config.h
+++ config.h
@@ -23,30 +23,36 @@ extern "C" {
 #define M_P_FORCE_OPTIMIZATION __attribute__((optimize("-Os")))
 //#define M_P_FORCE_OPTIMIZATION // uncomment this line instead for no optimizations
 #define M_P_ALLOW_QUIT
+#define M_P_PRINT_DEC_ON_STARTUP true
+
+
 //#define M_P_RETURN_AND_ARGUMENT_STRING_ENABLE
 #define M_P_RETURN_AND_ARGUMENT_UINT_ENABLE    // its size is target specific
 //#define M_P_RETURN_AND_ARGUMENT_UINT32_ENABLE
 //#define M_P_RETURN_AND_ARGUMENT_UINT64_ENABLE
-#define M_P_COLOR_ENABLE
-#define M_P_LIST_AND_AUTOCOMPLETE_ENABLE
-
-#ifdef M_P_LIST_AND_AUTOCOMPLETE_ENABLE
-#define M_P_AUTOCOMPLETE_ON_EMPTY_COMMANDS
-#endif
-
-#define M_P_PRINT_DEC_ON_STARTUP true
 
 #ifdef M_P_RETURN_AND_ARGUMENT_STRING_ENABLE
 #define M_P_RETURN_AND_ARGUMENT_STRING_BUFFER_SIZE 48
 #endif
 
+
+// Enable/disable the uptime and benchmark commands (see systick.c)
+//#define M_P_UPTIME
+
+#define M_P_COLOR_ENABLE
+
+#define M_P_LIST_AND_AUTOCOMPLETE_ENABLE
+#ifdef M_P_LIST_AND_AUTOCOMPLETE_ENABLE
+
+// Enable autocomplete even on completely empty command prompt
+#define M_P_AUTOCOMPLETE_ON_EMPTY_COMMANDS
+
 // Comment this out when you want the list and it's arguments be triggered
 // even when the command is already written in full
 //#define M_P_DO_NOT_LIST_ON_FULL_COMMANDS
 
+#endif // M_P_LIST_AND_AUTOCOMPLETE_ENABLE
 
-// Enable/disable the uptime and benchmark commands (see systick.c)
-//#define M_P_UPTIME
 
 #ifdef __cplusplus
 }
