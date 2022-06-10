51e600b18efe0a3f07b3253ffaf9c77ff87a3119
Anton Krug <antonkrug@antonkrug.eu>
2022-06-10 23:42:17 +0100

    Dedicated comments so the file can be edited programatically

--- commands_enabled.c
+++ commands_enabled.c
@@ -12,7 +12,9 @@
 #include "commands_enabled.h"
 
 #include "commands_bundled.h"
+// M_P_ADD_CUSTOM_COMMAND_INCLUDES_BEGIN
 #include "drivers/mss/mss_gpio_test_generic/mss_gpio_test_generic.h"
+// M_P_ADD_CUSTOM_COMMAND_INCLUDES_END
 
 #include <stdlib.h>
 
@@ -20,12 +22,15 @@ const m_p_group m_p_commands_enabled[] = {
         { "",               m_p_bundled_list },
         { "test_namespace", m_p_bundled_list },
 
-        // Add additional commands here to be accessible in all configurations
+        // M_P_ADD_CUSTOM_COMMAND_LIST_FOR_ALL_CONFIGURATIONS_BEGIN
         { "gpio",           m_p_gpio_list },
+        // M_P_ADD_CUSTOM_COMMAND_LIST_FOR_ALL_CONFIGURATIONS_END
 
-#ifndef NDEBUG
+#ifdef NDEBUG
         // Add additional commands here to be accessible only from Debug configurations
+        // M_P_ADD_CUSTOM_COMMAND_LIST_FOR_DEBUG_CONFIGURATIONS_BEGIN
 
+        // M_P_ADD_CUSTOM_COMMAND_LIST_FOR_DEBUG_CONFIGURATIONS_END
 #endif
         { NULL } // Terminator of the group
 };
